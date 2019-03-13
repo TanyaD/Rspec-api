b24a7e8e37e1fd31964c733e19cbb155afa52478
TanyaD <tat.dbk@gmail.com>
2019-03-13 14:04:55 -0700

    Add authenticate user to signin

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   namespace :api, path:"", defaults: {format: :json} do
     namespace :v1 do
         post "sign_up" => "registrations#create"
+        post "sign_in" => "sessions#create"
+        get "me" => "profile#show"
+        patch "me" => "profile#update"
     end
   end
 end
